1cf5d9dde4d2c0b10514140ca9df8d7d54ccdafd
Dorian Hidalgo <dorianhidalgo@hotmail.com>
2020-06-27 18:01:26 -0600

    Cambie color de fondo

--- lib/home.dart
+++ lib/home.dart
@@ -67,7 +67,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.green[300],
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
